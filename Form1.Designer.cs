e3295739486348654053b560f231c064f128196d
hcomilla <95985355+hcomilla@users.noreply.github.com>
2021-12-14 19:50:41 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace RoboticsIIFinalCountdownProjectExtraordinaire
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.sourcePictureBox = new System.Windows.Forms.PictureBox();
+            this.coloredPictureBox = new System.Windows.Forms.PictureBox();
+            this.radiusPictureBox = new System.Windows.Forms.PictureBox();
+            this.contourLabel = new System.Windows.Forms.Label();
+            this.coordinateLabel = new System.Windows.Forms.Label();
+            this.returnedPointLbl = new System.Windows.Forms.Label();
+            this.lockStateToolStripStatusLabel = new System.Windows.Forms.Label();
+            this.yInput = new System.Windows.Forms.TextBox();
+            this.xInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sourcePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coloredPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radiusPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(309, 318);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendBtn.TabIndex = 0;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
+            // 
+            // sourcePictureBox
+            // 
+            this.sourcePictureBox.Location = new System.Drawing.Point(12, 34);
+            this.sourcePictureBox.Name = "sourcePictureBox";
+            this.sourcePictureBox.Size = new System.Drawing.Size(288, 214);
+            this.sourcePictureBox.TabIndex = 1;
+            this.sourcePictureBox.TabStop = false;
+            // 
+            // coloredPictureBox
+            // 
+            this.coloredPictureBox.Location = new System.Drawing.Point(331, 34);
+            this.coloredPictureBox.Name = "coloredPictureBox";
+            this.coloredPictureBox.Size = new System.Drawing.Size(288, 214);
+            this.coloredPictureBox.TabIndex = 2;
+            this.coloredPictureBox.TabStop = false;
+            // 
+            // radiusPictureBox
+            // 
+            this.radiusPictureBox.Location = new System.Drawing.Point(653, 34);
+            this.radiusPictureBox.Name = "radiusPictureBox";
+            this.radiusPictureBox.Size = new System.Drawing.Size(288, 214);
+            this.radiusPictureBox.TabIndex = 3;
+            this.radiusPictureBox.TabStop = false;
+            // 
+            // contourLabel
+            // 
+            this.contourLabel.AutoSize = true;
+            this.contourLabel.Location = new System.Drawing.Point(40, 459);
+            this.contourLabel.Name = "contourLabel";
+            this.contourLabel.Size = new System.Drawing.Size(63, 17);
+            this.contourLabel.TabIndex = 4;
+            this.contourLabel.Text = "contours";
+            // 
+            // coordinateLabel
+            // 
+            this.coordinateLabel.AutoSize = true;
+            this.coordinateLabel.Location = new System.Drawing.Point(328, 459);
+            this.coordinateLabel.Name = "coordinateLabel";
+            this.coordinateLabel.Size = new System.Drawing.Size(82, 17);
+            this.coordinateLabel.TabIndex = 5;
+            this.coordinateLabel.Text = "coordinates";
+            // 
+            // returnedPointLbl
+            // 
+            this.returnedPointLbl.AutoSize = true;
+            this.returnedPointLbl.Location = new System.Drawing.Point(625, 348);
+            this.returnedPointLbl.Name = "returnedPointLbl";
+            this.returnedPointLbl.Size = new System.Drawing.Size(78, 17);
+            this.returnedPointLbl.TabIndex = 6;
+            this.returnedPointLbl.Text = "returnPoint";
+            // 
+            // lockStateToolStripStatusLabel
+            // 
+            this.lockStateToolStripStatusLabel.AutoSize = true;
+            this.lockStateToolStripStatusLabel.Location = new System.Drawing.Point(664, 459);
+            this.lockStateToolStripStatusLabel.Name = "lockStateToolStripStatusLabel";
+            this.lockStateToolStripStatusLabel.Size = new System.Drawing.Size(39, 17);
+            this.lockStateToolStripStatusLabel.TabIndex = 7;
+            this.lockStateToolStripStatusLabel.Text = "state";
+            // 
+            // yInput
+            // 
+            this.yInput.Location = new System.Drawing.Point(841, 367);
+            this.yInput.Name = "yInput";
+            this.yInput.Size = new System.Drawing.Size(100, 22);
+            this.yInput.TabIndex = 8;
+            // 
+            // xInput
+            // 
+            this.xInput.Location = new System.Drawing.Point(841, 319);
+            this.xInput.Name = "xInput";
+            this.xInput.Size = new System.Drawing.Size(100, 22);
+            this.xInput.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(814, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(814, 370);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Y:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 541);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.xInput);
+            this.Controls.Add(this.yInput);
+            this.Controls.Add(this.lockStateToolStripStatusLabel);
+            this.Controls.Add(this.returnedPointLbl);
+            this.Controls.Add(this.coordinateLabel);
+            this.Controls.Add(this.contourLabel);
+            this.Controls.Add(this.radiusPictureBox);
+            this.Controls.Add(this.coloredPictureBox);
+            this.Controls.Add(this.sourcePictureBox);
+            this.Controls.Add(this.sendBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sourcePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coloredPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radiusPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.PictureBox sourcePictureBox;
+        private System.Windows.Forms.PictureBox coloredPictureBox;
+        private System.Windows.Forms.PictureBox radiusPictureBox;
+        private System.Windows.Forms.Label contourLabel;
+        private System.Windows.Forms.Label coordinateLabel;
+        private System.Windows.Forms.Label returnedPointLbl;
+        private System.Windows.Forms.Label lockStateToolStripStatusLabel;
+        private System.Windows.Forms.TextBox yInput;
+        private System.Windows.Forms.TextBox xInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoboticsIIFinalCountdownProjectExtraordinaire
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture _capture;
+        Thread _captureThread;
+        SerialPort arduinoSerial = new SerialPort();
+        bool enableCoordinateSending = true;
+        Thread serialMonitoringThread;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ProcessImage()
+        {
+            while (_capture.IsOpened)
+            {
+                Mat workingImage = _capture.QueryFrame();
+                var imgConv = workingImage.ToImage<Bgr, byte>();
+                imgConv.ROI = new Rectangle(70, 60, workingImage.Width - 140, workingImage.Height - 120);
+                workingImage = imgConv.Mat;
+                // resize to PictureBox aspect ratio
+                int newHeight = workingImage.Size.Height * sourcePictureBox.Size.Width / workingImage.Size.Width;
+                Size newSize = new Size(sourcePictureBox.Size.Width, newHeight);
+                CvInvoke.Resize(workingImage, workingImage, newSize);
+                // as a test for comparison, create a copy of the image with a binary filter:
+                var binaryImage = workingImage.ToImage<Gray, byte>().ThresholdBinary(new Gray(125), new
+                Gray(255)).Mat;
+                // Sample for gaussian blur:
+                var blurredImage = new Mat();
+                var cannyImage = new Mat();
+                var decoratedImage = new Mat();
+                var radiusImage = new Mat(); 
+                decoratedImage = workingImage.Clone();
+                radiusImage = decoratedImage.Clone();
+                // find contours:
+                using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+                {
+                    // Build list of contours
+                    CvInvoke.FindContours(binaryImage, contours, null, RetrType.List, ChainApproxMethod.ChainApproxSimple);
+                    int radius = 3;
+                    int thickness = 1;
+                    int countedContours = 0;
+                    int scaledX = 0;
+                    int scaledY = 0;
+                    int sortingShapes = 0;
+                    for (int i = 0; i < contours.Size; i++)
+                    {
+                        VectorOfPoint contour = contours[i];
+                        double area = CvInvoke.ContourArea(contour);
+                        if (area > 2000 || area < 260) //mess with this for mounting camera
+                        {
+                            continue;
+                        }
+                        countedContours++;
+                        Rectangle boundingBox = CvInvoke.BoundingRectangle(contour);
+                        Point center = new Point(boundingBox.X + boundingBox.Width / 2, boundingBox.Y + boundingBox.Height / 2);
+                        scaledX = (int)Math.Round((center.X * 22) / (double)workingImage.Size.Width);
+                        scaledY = (int)Math.Round((center.Y * 17) / (double)workingImage.Size.Height);
+
+                        using (VectorOfPoint approxContour = new VectorOfPoint())
+                        {
+                            CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.05, true);
+                            // get an array of points in the contour
+                            Point[] points = approxContour.ToArray();
+                            // if array length isn't 4, something went wrong, abort warping process (for demo, draw points instead)
+                            if (points.Length == 4)
+                            {
+                                CvInvoke.Polylines(decoratedImage, contour, true, new Bgr(Color.Green).MCvScalar, 2);
+                                CvInvoke.Circle(radiusImage, center, radius, new Bgr(Color.Orange).MCvScalar, thickness);
+                                //categorizing shapes as 0 or 1 (0 is square, 1 is triangle)
+                                sortingShapes = 0;
+                            }
+                            else if (points.Length == 3)
+                            {
+                                CvInvoke.Polylines(decoratedImage, contour, true, new Bgr(Color.DeepPink).MCvScalar, 2);
+                                CvInvoke.Circle(radiusImage, center, radius, new Bgr(Color.Purple).MCvScalar, thickness);
+                                //categorizing shapes as 0 or 1 (0 is square, 1 is triangle)
+                                sortingShapes = 1;
+                            }
+                            else
+                            {
+                                CvInvoke.Polylines(decoratedImage, contour, true, new Bgr(Color.Red).MCvScalar, 2);
+                            }
+
+                    
+                        }
+
+                        //sending to arduino coordinates and if it is a sqaure or triangle
+                        if (enableCoordinateSending)
+                        {
+                            if (scaledX >= 0 && scaledY >= 0)
+                            {
+                                byte[] buffer = new byte[5]
+                                {
+                                Encoding.ASCII.GetBytes("<")[0],
+                                Convert.ToByte(sortingShapes),
+                                Convert.ToByte(scaledX),
+                                Convert.ToByte(scaledY),
+                                Encoding.ASCII.GetBytes(">")[0]
+                                };
+                                arduinoSerial.Write(buffer, 0, 5);
+                                enableCoordinateSending = false;
+                            }
+                            else
+                            {
+                                MessageBox.Show("X and Y values must be integers", "Unable to parse coordinates");
+                            }
+                        }
+                    }
+
+
+                    Invoke(new Action(() =>
+                    {
+                        contourLabel.Text = $"There are {countedContours} contours detected";
+                        coordinateLabel.Text = $"Coordinates: {scaledX},{scaledY} Shape: {sortingShapes}";
+                    }));
+                }
+                // output images:
+                sourcePictureBox.Image = workingImage.Bitmap;
+                coloredPictureBox.Image = decoratedImage.Bitmap;
+                radiusPictureBox.Image = radiusImage.Bitmap;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //1 is external camera and zero is laptop camera
+            _capture = new VideoCapture(1);
+            _captureThread = new Thread(ProcessImage);
+            _captureThread.Start();
+            try
+            {
+                arduinoSerial.PortName = "COM15";
+                arduinoSerial.BaudRate = 9600;
+                arduinoSerial.Open();
+                serialMonitoringThread = new Thread(MonitorSerialData);
+                serialMonitoringThread.Start();
+                xInput.Text = "130";
+                yInput.Text = "224";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error Initializing COM port");
+                Close();
+            }
+        }
+
+        private void MonitorSerialData()
+        {
+            while (true)
+            {
+                // block until \n character is received, extract command data
+                string msg = arduinoSerial.ReadLine();
+                // confirm the string has both < and > characters
+                if (msg.IndexOf("<") == -1 || msg.IndexOf(">") == -1)
+                {
+                    continue;
+                }
+                // remove everything before (and including) the < character
+                msg = msg.Substring(msg.IndexOf("<") + 1);
+                // remove everything after (and including) the > character
+                msg = msg.Remove(msg.IndexOf(">"));
+                // if the resulting string is empty, disregard and move on
+                if (msg.Length == 0)
+                {
+                    continue;
+                }
+                // parse the command
+                if (msg.Substring(0, 1) == "S")
+                {
+                    // command is to suspend, toggle states accordingly:
+                    ToggleFieldAvailability(msg.Substring(1, 1) == "1");
+                }
+                else if (msg.Substring(0, 1) == "P")
+                {
+                    // command is to display the point data, output to the text field:
+                    Invoke(new Action(() =>
+                    {
+                        returnedPointLbl.Text = $"Returned Point Data: {msg.Substring(1)}";
+                    }));
+                }
+            }
+        }
+        private void ToggleFieldAvailability(bool suspend)
+        {
+            Invoke(new Action(() =>
+            {
+                enableCoordinateSending = !suspend;
+                lockStateToolStripStatusLabel.Text = $"State: {(suspend ? "Locked" : "Unlocked")}";
+            }));
+        }
+        
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            serialMonitoringThread.Abort();
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            if (!enableCoordinateSending)
+            {
+                MessageBox.Show("Temporarily locked...");
+                return;
+            }
+            int x = -1;
+            int y = -1;
+            if (int.TryParse(xInput.Text, out x) && int.TryParse(yInput.Text, out y))
+            {
+                byte[] buffer = new byte[5] {
+                    Encoding.ASCII.GetBytes("<")[0],
+                    Convert.ToByte(0),
+                    Convert.ToByte(x),
+                    Convert.ToByte(y),
+                    Encoding.ASCII.GetBytes(">")[0]
+                };
+                arduinoSerial.Write(buffer, 0, 5);
+            }
+            else
+            {
+                MessageBox.Show("X and Y values must be integers", "Unable to parse coordinates");
+            }
+        }
+    }
+}
